857f8146cedea01a9ae16dea7061325828559d47
Vlad <mitskevichvlm@gmail.com>
2023-10-25 16:50:51 +0300

    feat: check scope prototype and singleton

--- src/main/java/by/bat/Dog.java
+++ src/main/java/by/bat/Dog.java
@@ -1,8 +1,21 @@
 package by.bat;
 
 public class Dog implements IPet{
+    private String name;
+    public Dog() {
+        System.out.println("bean for dog");
+    }
+
     @Override
     public void say() {
         System.out.println("Bow-Wow");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
---
+++ src/main/java/by/bat/Test4.java
@@ -0,0 +1,25 @@
+package by.bat;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test4 {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("applicatiomContext2.xml");
+
+        Dog myDog = context.getBean("myPet", Dog.class);
+        //  myDog.setName("jojo");
+        Dog yourDog = context.getBean("myPet", Dog.class);
+        //   yourDog.setName("belka");
+
+        //  System.out.println(myDog.getName());
+        //  System.out.println(yourDog.getName());
+
+        System.out.println(myDog == yourDog);
+
+        System.out.println(myDog);
+        System.out.println(yourDog);
+
+        context.close();
+    }
+}
