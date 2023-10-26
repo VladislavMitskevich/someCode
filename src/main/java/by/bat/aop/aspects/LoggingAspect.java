e319776368fe1988152cc88d0bc75120719fe473
Vlad <mitskevichvlm@gmail.com>
2023-10-26 22:06:57 +0300

    feat: add Aspect and Pointcut

--- src/main/java/by/bat/aop/aspects/LoggingAspect.java
+++ src/main/java/by/bat/aop/aspects/LoggingAspect.java
@@ -1,8 +1,15 @@
 package by.bat.aop.aspects;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 @Component
 @Aspect
 public class LoggingAspect {
+
+    @Before("execution(public void getBook())")
+    public void beforeGetBookAdvice(){
+        System.out.println("try to get book");
+    }
 }
